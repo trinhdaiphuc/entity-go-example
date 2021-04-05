2cb8a36b89441b229a47f637b19f8928800ac3b7
trinhdaiphuc <trinhdaiphuc@gmail.com>
2021-04-06 00:51:00 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module trinhdaiphuc/ent-turtorial
+
+go 1.16
+
+require (
+	entgo.io/ent v0.7.0
+	github.com/go-sql-driver/mysql v1.5.1-0.20200311113236-681ffa848bae
+)
---
+++ main.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"entgo.io/ent/dialect"
+	_ "github.com/go-sql-driver/mysql"
+	"trinhdaiphuc/ent-turtorial/ent"
+	"trinhdaiphuc/ent-turtorial/ent/car"
+	"trinhdaiphuc/ent-turtorial/ent/group"
+	"trinhdaiphuc/ent-turtorial/ent/user"
+)
+
+func CreateGraph(ctx context.Context, client *ent.Client) error {
+	// First, create the users.
+	a8m, err := client.User.
+		Create().
+		SetAge(30).
+		SetName("Ariel").
+		Save(ctx)
+	if err != nil {
+		return err
+	}
+	neta, err := client.User.
+		Create().
+		SetAge(28).
+		SetName("Neta").
+		Save(ctx)
+	if err != nil {
+		return err
+	}
+	// Then, create the cars, and attach them to the users in the creation.
+	_, err = client.Car.
+		Create().
+		SetModel("Tesla").
+		SetRegisteredAt(time.Now()). // ignore the time in the graph.
+		SetOwner(a8m). // attach this graph to Ariel.
+		Save(ctx)
+	if err != nil {
+		return err
+	}
+	_, err = client.Car.
+		Create().
+		SetModel("Mazda").
+		SetRegisteredAt(time.Now()). // ignore the time in the graph.
+		SetOwner(a8m). // attach this graph to Ariel.
+		Save(ctx)
+	if err != nil {
+		return err
+	}
+	_, err = client.Car.
+		Create().
+		SetModel("Ford").
+		SetRegisteredAt(time.Now()). // ignore the time in the graph.
+		SetOwner(neta). // attach this graph to Neta.
+		Save(ctx)
+	if err != nil {
+		return err
+	}
+	// Create the groups, and add their users in the creation.
+	_, err = client.Group.
+		Create().
+		SetName("GitLab").
+		AddUsers(neta, a8m).
+		Save(ctx)
+	if err != nil {
+		return err
+	}
+	_, err = client.Group.
+		Create().
+		SetName("GitHub").
+		AddUsers(a8m).
+		Save(ctx)
+	if err != nil {
+		return err
+	}
+	log.Println("The graph was created successfully")
+	return nil
+}
+
+func QueryGithub(ctx context.Context, client *ent.Client) error {
+	cars, err := client.Group.
+		Query().
+		Where(group.Name("GitHub")). // (Group(Name=GitHub),)
+		QueryUsers(). // (User(Name=Ariel, Age=30),)
+		QueryCars(). // (Car(Model=Tesla, RegisteredAt=<Time>), Car(Model=Mazda, RegisteredAt=<Time>),)
+		All(ctx)
+	if err != nil {
+		return fmt.Errorf("failed getting cars: %w", err)
+	}
+	log.Println("cars returned:", cars)
+	// Output: (Car(Model=Tesla, RegisteredAt=<Time>), Car(Model=Mazda, RegisteredAt=<Time>),)
+	return nil
+}
+
+func QueryArielCars(ctx context.Context, client *ent.Client) error {
+	// Get "Ariel" from previous steps.
+	a8m := client.User.
+		Query().
+		Where(
+			user.HasCars(),
+			user.Name("Ariel"),
+		).
+		OnlyX(ctx)
+	cars, err := a8m. // Get the groups, that a8m is connected to:
+		QueryGroups(). // (Group(Name=GitHub), Group(Name=GitLab),)
+		QueryUsers(). // (User(Name=Ariel, Age=30), User(Name=Neta, Age=28),)
+		QueryCars(). //
+		Where( //
+			car.Not( //  Get Neta and Ariel cars, but filter out
+				car.ModelEQ("Mazda"), //  those who named "Mazda"
+			), //
+		). //
+		All(ctx)
+	if err != nil {
+		return fmt.Errorf("failed getting cars: %w", err)
+	}
+	log.Println("cars returned:", cars)
+	// Output: (Car(Model=Tesla, RegisteredAt=<Time>), Car(Model=Ford, RegisteredAt=<Time>),)
+	return nil
+}
+
+func QueryGroupWithUsers(ctx context.Context, client *ent.Client) error {
+	groups, err := client.Group.
+		Query().
+		Where(group.HasUsers()).
+		All(ctx)
+	if err != nil {
+		return fmt.Errorf("failed getting groups: %w", err)
+	}
+	log.Println("groups returned:", groups)
+	// Output: (Group(Name=GitHub), Group(Name=GitLab),)
+	return nil
+}
+
+func main() {
+	dbName := "test"
+	client, err := ent.Open(dialect.MySQL,
+		fmt.Sprintf("root:password@tcp(127.0.0.1:3306)/%v?parseTime=true", dbName),
+	)
+	if err != nil {
+		log.Fatalf("failed opening connection to sqlite: %v", err)
+	}
+	defer client.Close()
+
+	ctx := context.Background()
+	// Run the auto migration tool.
+	if err := client.Schema.Create(ctx); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+
+	CreateGraph(ctx, client)
+	QueryGithub(ctx, client)
+	QueryArielCars(ctx, client)
+	QueryGroupWithUsers(ctx, client)
+}
